49063a82290156179d691d6e196fea09627cde23
Dan Siegel <me@dansiegel.net>
2019-05-09 09:48:11 -0700

    updating Shiny docs

---
+++ src/Shiny.Prism.DryIoc/PrismApplication.cs
@@ -0,0 +1,25 @@
+﻿using Prism;
+using Prism.DryIoc;
+using Prism.Ioc;
+using Xamarin.Forms;
+
+[assembly: XmlnsDefinition("http://prismlibrary.com", "Shiny.Prism.DryIoc")]
+namespace Shiny.Prism.DryIoc
+{
+    public abstract class PrismApplication : PrismApplicationBase
+    {
+        public PrismApplication()
+        {
+        }
+
+        public PrismApplication(IPlatformInitializer platformInitializer) : base(platformInitializer)
+        {
+        }
+
+        public PrismApplication(IPlatformInitializer platformInitializer, bool setFormsDependencyResolver) : base(platformInitializer, setFormsDependencyResolver)
+        {
+        }
+
+        protected override IContainerExtension CreateContainerExtension() => PrismContainerExtension.Current;
+    }
+}
