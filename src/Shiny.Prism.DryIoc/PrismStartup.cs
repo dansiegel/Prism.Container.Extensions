dd77ff4b7f2db09fcfede9fbaba591c725919efd
Dan Siegel <me@dansiegel.net>
2019-05-09 09:18:42 -0700

    Adding Shiny Prism App

---
+++ src/Shiny.Prism.DryIoc/PrismStartup.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Prism.DryIoc;
+using Prism.Ioc;
+using System;
+
+namespace Shiny.Prism.DryIoc
+{
+    public abstract class PrismStartup : Startup
+    {
+        public override sealed IServiceProvider CreateServiceProvider(IServiceCollection services)
+        {
+            return CreateContainerExtension().CreateServiceProvider(services);
+        }
+
+        protected virtual IContainerExtension CreateContainerExtension() => PrismContainerExtension.Current;
+    }
+}
