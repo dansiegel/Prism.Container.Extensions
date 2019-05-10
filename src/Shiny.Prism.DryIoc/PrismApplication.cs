912a7f1a0b8f1f5c22e88c6bfc61b3e9bbf986dc
Dan Siegel <me@dansiegel.net>
2019-05-10 07:45:51 -0700

    adding ILogger

--- src/Shiny.Prism.DryIoc/PrismApplication.cs
+++ src/Shiny.Prism.DryIoc/PrismApplication.cs
@@ -1,6 +1,7 @@
 ﻿using Prism;
 using Prism.DryIoc;
 using Prism.Ioc;
+using Prism.Logging;
 using Xamarin.Forms;
 
 [assembly: XmlnsDefinition("http://prismlibrary.com", "Shiny.Prism.DryIoc")]
@@ -20,6 +21,20 @@ namespace Shiny.Prism.DryIoc
         {
         }
 
+        public ILogger Logger { get; private set; }
+
         protected override IContainerExtension CreateContainerExtension() => PrismContainerExtension.Current;
+
+        public override void Initialize()
+        {
+            base.Initialize();
+            Logger = Container.Resolve<ILogger>();
+        }
+
+        protected override sealed void RegisterRequiredTypes(IContainerRegistry containerRegistry)
+        {
+            containerRegistry.RegisterManySingleton<ConsoleLoggingService>();
+            base.RegisterRequiredTypes(containerRegistry);
+        }
     }
 }
