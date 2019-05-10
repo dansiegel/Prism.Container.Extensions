62448d32e25cf6978ac3e16b5aaa5d53bb33c820
Dan Siegel <me@dansiegel.net>
2019-05-10 08:04:29 -0700

    add error handling

--- src/Shiny.Prism.DryIoc/PrismApplication.cs
+++ src/Shiny.Prism.DryIoc/PrismApplication.cs
@@ -1,8 +1,13 @@
-﻿using Prism;
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Prism;
 using Prism.DryIoc;
 using Prism.Ioc;
 using Prism.Logging;
+using Prism.Modularity;
 using Xamarin.Forms;
+using Xamarin.Forms.Internals;
 
 [assembly: XmlnsDefinition("http://prismlibrary.com", "Shiny.Prism.DryIoc")]
 namespace Shiny.Prism.DryIoc
@@ -29,12 +34,80 @@ namespace Shiny.Prism.DryIoc
         {
             base.Initialize();
             Logger = Container.Resolve<ILogger>();
+            Log.Listeners.Add(Container.Resolve<FormsLogListener>());
+
+            AppDomain.CurrentDomain.UnhandledException += AppDomain_UnhandledException;
+
+            TaskScheduler.UnobservedTaskException += TaskScheduler_UnobservedTaskException;
+
+            Container.Resolve<IModuleManager>().LoadModuleCompleted += PrismApplication_LoadModuleCompleted;
         }
 
         protected override sealed void RegisterRequiredTypes(IContainerRegistry containerRegistry)
         {
             containerRegistry.RegisterManySingleton<ConsoleLoggingService>();
             base.RegisterRequiredTypes(containerRegistry);
+        }
+
+        private void PrismApplication_LoadModuleCompleted(object sender, LoadModuleCompletedEventArgs e)
+        {
+            LoadModuleCompleted(e.ModuleInfo, e.Error, e.IsErrorHandled);
+        }
+
+        protected virtual void LoadModuleCompleted(IModuleInfo moduleInfo, Exception error, bool isHandled)
+        {
+            if(error != null)
+            {
+                Logger.Debug($"An error occurred while loading {moduleInfo.ModuleName}");
+                Logger.Report(error, new Dictionary<string, string>
+                {
+                    { "moduleName", moduleInfo.ModuleName },
+                    { "initializationMode", $"{moduleInfo.InitializationMode}" },
+                    { "moduleType", moduleInfo.ModuleType }
+                });
+            }
+        }
+
+        private void AppDomain_UnhandledException(object sender, UnhandledExceptionEventArgs args)
+        {
+            if (args.ExceptionObject is Exception ex)
+                TrackError(ex, nameof(AppDomain_UnhandledException));
+            else
+                TrackError(null, nameof(AppDomain_UnhandledException), args.ExceptionObject);
+        }
+
+        private void TaskScheduler_UnobservedTaskException(object sender, UnobservedTaskExceptionEventArgs args)
+        {
+            args.SetObserved();
+            TrackError(args.Exception, nameof(TaskScheduler_UnobservedTaskException));
+        }
+
+        protected virtual void TrackError(Exception ex, string fromEvent, object errorObject = null)
+        {
+            if (errorObject != null)
+            {
+                System.Diagnostics.Trace.WriteLine(errorObject);
+            }
+            else
+            {
+                System.Diagnostics.Trace.WriteLine(ex);
+                Logger.Report(ex, new Dictionary<string, string> { { "fromUnobservedEvent", fromEvent } });
+            }
+        }
+
+        class FormsLogListener : LogListener
+        {
+            private ILogger Logger { get; }
+
+            public FormsLogListener(ILogger logger)
+            {
+                Logger = logger;
+            }
+
+            public override void Warning(string category, string message)
+            {
+                Logger.Debug($"{category}: {message}");
+            }
         }
     }
 }
