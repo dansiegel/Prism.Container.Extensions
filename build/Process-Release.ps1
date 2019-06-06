1603830bb6c202bea13fd4112efa4c720d9b7f59
Dan Siegel <me@dansiegel.net>
2019-06-05 20:53:41 -0700

    catch exceptions

--- build/Process-Release.ps1
+++ build/Process-Release.ps1
@@ -1,34 +1,39 @@
-$location = Get-Location
-$currentDirectory = $location.Path
+try {
+    $artifactDirectory = $env:Pipeline_Workspace
 
-Write-Host "Currect working directory: $currentDirectory"
+    Write-Host "Currect working directory: $artifactDirectory"
 
-$nupkg = Get-ChildItem -Path $currentDirectory -Filter *.nupkg -Recurse | Select-Object -First 1
+    $nupkg = Get-ChildItem -Path $artifactDirectory -Filter *.nupkg -Recurse | Select-Object -First 1
 
-if($nupkg -eq $null)
-{
-    Throw "No NuGet Package could be found in the current directory"
+    if($null -eq $nupkg)
+    {
+        Throw "No NuGet Package could be found in the current directory"
+    }
+
+    Write-Host "Package Name: $($nupkg.Name)"
+    $nupkg.Name -match '^(.*?)\.((?:\.?[0-9]+){3,}(?:[-a-z]+)?)\.nupkg$'
+
+    $VersionName = $Matches[2]
+    $IsPreview = $VersionName -match '-pre$'
+    $ReleaseDisplayName = $VersionName
+
+    if($null -eq $env:IS_PREVIEW)
+    {
+        Write-Output ("##vso[task.setvariable variable=IS_PREVIEW;]$IsPreview")
+    }
+
+    if($true -eq $IsPreview)
+    {
+        $ReleaseDisplayName = "$VersionName - Preview"
+    }
+
+    Write-Host "Version Name" $VersionName
+    Write-Host "Release Display Name: $ReleaseDisplayName"
+
+    Write-Output ("##vso[task.setvariable variable=VersionName;]$VersionName")
+    Write-Output ("##vso[task.setvariable variable=ReleaseDisplayName;]$ReleaseDisplayName")
 }
-
-Write-Host "Package Name: $($nupkg.Name)"
-$nupkg.Name -match '^(.*?)\.((?:\.?[0-9]+){3,}(?:[-a-z]+)?)\.nupkg$'
-
-$VersionName = $Matches[2]
-$IsPreview = $VersionName -match '-pre$'
-$ReleaseDisplayName = $VersionName
-
-if($env:IS_PREVIEW -eq $null)
-{
-    Write-Output ("##vso[task.setvariable variable=IS_PREVIEW;]$IsPreview")
+catch {
+    Write-Error  $_
+    exit 1
 }
-
-if($IsPreview -eq $true)
-{
-    $ReleaseDisplayName = "$VersionName - Preview"
-}
-
-Write-Host "Version Name" $VersionName
-Write-Host "Release Display Name: $ReleaseDisplayName"
-
-Write-Output ("##vso[task.setvariable variable=VersionName;]$VersionName")
-Write-Output ("##vso[task.setvariable variable=ReleaseDisplayName;]$ReleaseDisplayName")
