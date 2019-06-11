11ebb15bb1c841fdfc2c366b8d305b4f49f94bfa
Dan Siegel <me@dansiegel.net>
2019-06-10 19:35:02 -0700

    make Shiny generic

---
+++ src/Shiny.Prism/PrismStartup.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Prism.Ioc;
+using System;
+
+namespace Shiny.Prism
+{
+    public abstract class PrismStartup : IStartup
+    {
+        private IContainerExtension _container;
+
+        protected PrismStartup()
+        {
+        }
+
+        protected PrismStartup(IContainerExtension container)
+        {
+            _container = container;
+        }
+
+        public abstract void ConfigureServices(IServiceCollection services);
+
+        void IStartup.ConfigureApp(IServiceProvider provider) { }
+
+        IServiceProvider IStartup.CreateServiceProvider(IServiceCollection services)
+        {
+            return CreateContainerExtension().CreateServiceProvider(services);
+        }
+
+        protected virtual IContainerExtension CreateContainerExtension() => _container;
+
+        public IStartup WithContainer(IContainerExtension container)
+        {
+            _container = container;
+            return this;
+        }
+    }
+}
