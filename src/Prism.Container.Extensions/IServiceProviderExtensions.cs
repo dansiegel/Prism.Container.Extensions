22ba0d4544268f5266133c8ae762b7abb89462f8
Dan Siegel <me@dansiegel.net>
2019-07-25 08:11:43 -0700

    enusre we always register and pass back a single instance of the IServiceProvider

--- src/Prism.Container.Extensions/IServiceProviderExtensions.cs
+++ src/Prism.Container.Extensions/IServiceProviderExtensions.cs
@@ -5,7 +5,7 @@ namespace Prism.Ioc
 {
     public static class IServiceProviderExtensions
     {
-        public static IServiceProvider CreateServiceProvider(this IContainerProvider container, IServiceCollection services)
+        public static IServiceProvider CreateServiceProvider(this IContainerExtension container, IServiceCollection services)
         {
             var containerRegistry = container as IContainerRegistry;
             RegisterTypesWithPrismContainer(containerRegistry, services);
@@ -16,7 +16,7 @@ namespace Prism.Ioc
                 containerRegistry.RegisterInstance<IServiceProvider>(serviceProvider);
             }
 
-            return serviceProvider;
+            return container.Resolve<IServiceProvider>();
         }
 
         private static void RegisterTypesWithPrismContainer(IContainerRegistry containerRegistry, IServiceCollection services)
@@ -41,10 +41,11 @@ namespace Prism.Ioc
                         // Transient Lifetime cannot occur with an Instance
                         break;
                     case ServiceLifetime.Scoped:
-                        if (service.ImplementationType is null)
-                            containerRegistry.RegisterScoped(service.ServiceType);
-                        else
+                        if (service.ImplementationType != null)
                             containerRegistry.RegisterScoped(service.ServiceType, service.ImplementationType);
+                        else if (service.ImplementationType is null)
+                            containerRegistry.RegisterScoped(service.ServiceType);
+                        // Everything else is currently unsupported...
                         break;
                 }
             }
