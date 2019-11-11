2a5e103e67f4e2cae9c26d3179e1c6bbcf62e632
Dan Siegel <me@dansiegel.net>
2019-11-11 11:24:04 -0800

    updating NuGet info

---
+++ src/Prism.Microsoft.DependencyInjection.Extensions/MicrsoftDependencyInjectionExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using Microsoft.Extensions.DependencyInjection;
+using Prism.Ioc;
+
+[assembly: InternalsVisibleTo("Prism.Microsoft.DependencyInjection.Extensions.Tests")]
+[assembly: InternalsVisibleTo("Prism.Microsoft.DependencyInjection.Forms.Extended.Tests")]
+[assembly: InternalsVisibleTo("Shiny.Prism.Tests")]
+namespace Prism.Microsoft.DependencyInjection
+{
+    public static class MicrsoftDependencyInjectionExtensions
+    {
+        public static IServiceCollection ServiceCollection(this IContainerRegistry containerRegistry)
+        {
+            if(containerRegistry is PrismContainerExtension pce)
+            {
+                return pce.Services;
+            }
+
+            throw new NotImplementedException("IContainerRegistry must be implmented from the concrete type PrismContainerExtension");
+        }
+    }
+}
