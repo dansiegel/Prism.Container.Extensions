f5632b2fbf2f0484e9a1cfae8af5cfbfa6667b4a
Dan Siegel <me@dansiegel.net>
2019-12-03 11:23:53 -0800

    adding sample project

---
+++ sample/PrismSample/Shiny/Startup.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Extensions.DependencyInjection;
+using Shiny.Prism;
+using Shiny.Power;
+
+namespace PrismSample.Shiny
+{
+    public class Startup : PrismStartup
+    {
+        protected override void ConfigureServices(IServiceCollection services)
+        {
+            // Register Stuff
+        }
+    }
+}
---
+++ sample/PrismSample/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Prism.Mvvm;
+using Shiny.Net;
+
+namespace PrismSample.ViewModels
+{
+    public class MainPageViewModel : BindableBase
+    {
+        private IConnectivity _connectivity { get; set; }
+
+        public MainPageViewModel(IConnectivity connectivity)
+        {
+            _connectivity = connectivity;
+            _connectivity.WhenInternetStatusChanged()
+                .Subscribe(OnConnectivityChanged);
+
+            OnConnectivityChanged(_connectivity.IsInternetAvailable());
+        }
+
+        private string _message;
+        public string Message
+        {
+            get => _message;
+            set => SetProperty(ref _message, value);
+        }
+
+        private void OnConnectivityChanged(bool connected)
+        {
+            if(connected)
+            {
+                Message = "The internet is connected... We can now do our anti-Social Media... and swipe right, and like our friend's lunch...";
+            }
+            else
+            {
+                Message = "Whoops! It seems the internet has gone missing... we're going into withdrawls... please bring it back...";
+            }
+        }
+    }
+}
