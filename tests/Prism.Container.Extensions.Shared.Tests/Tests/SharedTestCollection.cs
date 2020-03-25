0e69396c5c82c6121a4ae7f48b67ab9883211352
m.volkau <maksim.volkau@adform.com>
2020-03-25 16:57:57 +0300

    explicitly disable parallelization for the collection, see https://xunit.net/docs/running-tests-in-parallel.html

--- tests/Prism.Container.Extensions.Shared.Tests/Tests/SharedTestCollection.cs
+++ tests/Prism.Container.Extensions.Shared.Tests/Tests/SharedTestCollection.cs
@@ -1,13 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using Xunit;
+﻿using Xunit;
 
 namespace Prism.Container.Extensions.Shared.Tests
 {
     public class SharedTests { }
 
-    [Collection(nameof(SharedTests))]
+    [CollectionDefinition(nameof(SharedTests), DisableParallelization = true)]
     public class SharedTestCollection : ICollectionFixture<SharedTests>
     {
     }
