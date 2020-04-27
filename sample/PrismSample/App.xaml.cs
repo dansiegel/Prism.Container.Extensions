d5c492177b9114bf32bc43367fde562fbc2cc698
Dan Siegel <me@dansiegel.net>
2020-04-26 17:02:36 -0700

    prism 8 updates

--- sample/PrismSample/App.xaml.cs
+++ sample/PrismSample/App.xaml.cs
@@ -1,24 +1,34 @@
 ﻿using System;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using Humanizer;
 using Prism.Ioc;
+using Prism.Navigation;
 using PrismSample.ViewModels;
 using PrismSample.Views;
 using Xamarin.Forms;
 
 namespace PrismSample
 {
+    [AutoRegisterForNavigation]
     public partial class App
     {
         public App()
         {
         }
 
-        protected override async void OnInitialized()
+        protected override void OnInitialized()
         {
             InitializeComponent();
 
-            var result = await NavigationService.NavigateAsync("MainPage");
+            NavigationService.NavigateAsync("navigation/main").Await(OnNavigationResult);
 
-            if(!result.Success)
+            
+        }
+
+        private void OnNavigationResult(INavigationResult result)
+        {
+            if (!result.Success)
             {
                 MainPage = new MainPage
                 {
@@ -33,8 +43,16 @@ namespace PrismSample
 
         protected override void RegisterTypes(IContainerRegistry containerRegistry)
         {
-            containerRegistry.RegisterForNavigation<NavigationPage>();
-            containerRegistry.RegisterForNavigation<MainPage, MainPageViewModel>();
+           // var isRegistered = containerRegistry.IsRegistered<INavigationService>(App.NavigationServiceName);
+            System.Diagnostics.Debugger.Break();
+            //containerRegistry.RegisterForNavigation<NavigationPage>();
+            //containerRegistry.RegisterForNavigation<MainPage, MainPageViewModel>();
+        }
+
+        protected override string GetNavigationSegmentNameFromType(Type pageType)
+        {
+            var name = Regex.Replace(pageType.Name, "Page$", string.Empty).Camelize();
+            return name;
         }
     }
 }
